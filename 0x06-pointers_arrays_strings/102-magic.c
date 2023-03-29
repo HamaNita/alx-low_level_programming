cbfe17aecc5c78cdd75483736e0899d9e6e442c6
HamaNita <hama21anitah@gmail.com>
2023-03-29 11:02:31 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,6 +9,8 @@ int *p;
 a[2] = 1024;
 p = &n;
   /*
+   * main - this is a main fuction
+   * description - the starting point of the program
    * write your line of code here...
    * Remember:
    * - you are not allowed to use a
